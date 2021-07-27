45b45c6e618bf970b82c1f809a5b6fe57457a532
sahil2501 <82953793+sahil2501@users.noreply.github.com>
2021-07-27 10:57:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function big() {
+    document.getElementById("button").style.fontSize = 30;
+}
+function small() {
+    document.getElementById("button").style.fontSize = 20;
+}
+function play_sound() {
+    document.getElementById("text").innerHTML = "HAPPY BIRTH DAY!";
+    document.getElementById("text").style.fontSize = 100;
+    document.getElementById("text").style.color = "darkviolet";
+    x.play();
+}
